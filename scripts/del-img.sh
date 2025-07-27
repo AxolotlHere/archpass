81e9f279aa20831b4b4b23927991aee774581f73
AxolotlHere <snaveenbharath2005@gmail.com>
2025-07-27 11:33:04 +0530

    fix: base img edge case for del-img

--- scripts/del-img.sh
+++ scripts/del-img.sh
@@ -1,5 +1,9 @@
 rm /usr/archpass/img/$1.qcow2
 
+if [$1 -eq "archpass-base-img"]; then 
+  echo "archpass: cant delete archpass-base-img, terminating process"
+  exit 4 
+
 if [ $? -eq 0 ]; then
   echo "archpass: Image deleted successfully"
 fi
